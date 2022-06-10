1c01d9d8a6f327f0154d6ffe0ab1bc82c2f56e56
rahulsolanki <rahulsolanki.magneto@gmail.com>
2022-06-10 22:37:18 +0530

    firstt

--- lib/View/Screens/body_screen.dart
+++ lib/View/Screens/body_screen.dart
@@ -19,15 +19,58 @@ class _BodyScreenState extends State<BodyScreen> {
         backgroundColor: ColorConstants.primaryBlueColor,
         elevation: 4,
         centerTitle: true,
-        title:Text("Home",style: GoogleFonts.heebo(fontWeight: FontWeight.normal),),
+        title: Text(
+          "Home",
+          style: GoogleFonts.heebo(fontWeight: FontWeight.normal),
+        ),
         actions: [
-          SvgPicture.asset("assets/images/avatar.svg",),
+          SvgPicture.asset(
+            "assets/images/avatar.svg",
+          ),
           Container(
             width: 5,
           )
         ],
       ),
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 16,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Diagnosis History",
+                  style: GoogleFonts.heebo(
+                      fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                Container(
+                  width: 100,
+
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: ColorConstants.primaryBlueColor, //                   <--- border color
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                    height: 50,
+                    child: Center(
+                        child: Text(
+                      "Backside",
+                      style: GoogleFonts.roboto(fontSize: 16,fontWeight: FontWeight.w700,color: ColorConstants.primaryBlueColor),
+                    ))),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: ColorConstants.primaryBlueColor,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
-
